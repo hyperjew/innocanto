e666a2a10f7150e86e82bcf7ad0de91668749928
hyperjew <garciajewels11@gmail.com>
2023-04-23 21:36:29 +0800

    phpmailer edit 2

--- send.php
+++ send.php
@@ -1,11 +1,52 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
+
+// if(isset($_POST['name']) && isset($_POST['email'])){
+//     $name = $_POST['name'];
+//     $email = $_POST['email'];
+//     $message = $_POST['message'];
+
+//     require_once 'phpmailer/src/PHPMailer.php';
+//     require_once 'phpmailer/src/SMTP.php';
+//     require_once 'phpmailer/src/Exception.php';
+
+//     $mail = new PHPMailer();
+
+//     // smtp settings
+//     $mail->isSMTP();
+//     $mail->Host = 'smtp.gmail.com';
+//     $mail->SMTPAuth = true;
+//     $mail->Username = 'carpoolkachow@gmail.com';
+//     $mail->Password = 'lndgepzfhhwhbnwc';
+//     $mail->SMTPSecure = 'ssl';
+//     $mail->Port = 465;
+
+//     // email settings
+//     $mail->isHTML(true);
+//     $mail->setFrom($email, $name);
+//     $mail->addAddress("carpoolkachow@gmail.com");
+//     $mail->Body = $message;
+
+//     if($mail->send()) {
+//         $status = "success";
+//         $response = "Feedback is sent!";
+//     }
+//     else
+//     {
+//         $status = "failed";
+//         $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+//     }
+
+//     exit(json_encode(array("status" => $status, "response" => $response)));
+
+// }
+
 
 if(isset($_POST['send'])){
     $mail = new PHPMailer(true);
@@ -18,9 +59,8 @@ if(isset($_POST['send'])){
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
-    $mail->setFrom('carpoolkachow@gmail.com');
-
-    $mail->addAddress($_POST['email']);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("carpoolkachow@gmail.com");
 
     $mail->isHTML(true);
 
