862326dac45dfb475c0338cdab79e9e74d6575e7
hyperjew <garciajewels11@gmail.com>
2023-04-23 21:00:58 +0800

    phpmailer edit

--- send.php
+++ send.php
@@ -7,7 +7,7 @@ require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
-if(isset($_POST["send"])){
+if(isset($_POST['send'])){
     $mail = new PHPMailer(true);
 
     $mail->isSMTP();
@@ -15,27 +15,27 @@ if(isset($_POST["send"])){
     $mail->SMTPAuth = true;
     $mail->Username = 'carpoolkachow@gmail.com';
     $mail->Password = 'lndgepzfhhwhbnwc';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
     $mail->setFrom('carpoolkachow@gmail.com');
 
-    $mail->addAddress($_POST["email"]);
+    $mail->addAddress($_POST['email']);
 
     $mail->isHTML(true);
 
-    $mail->Subject = $_POST["name"];
-    $mail->Body = $_POST["message"];
+    $mail->Subject = $_POST['name'];
+    $mail->Body = $_POST['message'];
 
     $mail->send();
 
-    echo
-    "
-    <script>
-    alert('Sent Successfully');
-    document.location.href = index.html
-    </script>
-    ";
+    // echo
+    // "
+    // <script>
+    // alert('Sent Successfully');
+    // document.location.href = index.php
+    // </script>
+    // ";
 }
 ?>
 
